e45ee992a5fae488e0f0cc6b1266fa5165a5fbc9
okarpuk <oleg.karpuk.1982@gmail.com>
2023-03-14 23:00:28 +0300

    Solution structure

---
+++ HomeTask_5_Phones_CreditCards/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeTask_5_Phones_CreditCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ HomeTask_6_Figures_Hospital/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeTask_6_Figures_Hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
