0061be57e5b3e43d8a1e703e78693636830a7a6f
okarpuk <oleg.karpuk.1982@gmail.com>
2023-03-15 21:46:25 +0300

    Task 2 added

---
+++ HomeTask_7_AutoPark_Cars/Cars/Bike.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_7_AutoPark_Cars.Cars
+{
+    internal class Bike : CarInfo
+    {
+        public bool SidecarIsAble { get; set; }
+
+        public override void PrintVehicleInfo()
+        {
+            base.PrintVehicleInfo();
+            Console.WriteLine($"SidecarIsAble = {SidecarIsAble}");
+        }
+
+        public override int GetMaxLoad()
+        {
+            return SidecarIsAble ? this.MaxLoad : 0;
+        }
+    }
+}
+
--- HomeTask_7_AutoPark_Cars/Program.cs
+++ HomeTask_7_AutoPark_Cars/Program.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using HomeTask_7_AutoPark_Cars.AutoPark;
+using HomeTask_7_AutoPark_Cars.Cars;
 
 namespace HomeTask_7_AutoPark_Cars
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {/*
             //AUTOPARK
             //BUS
             CityBus cBus1 = new CityBus("Railway station", "3", "09:00", 30);
@@ -59,9 +63,45 @@ namespace HomeTask_7_AutoPark_Cars
                     Console.WriteLine(string.Format("{0}, {1}, {2}", searchRoute2.RouteNumber, searchRoute2.Destination, searchRoute2.DepartureTime));
                 }
             }
+            */
 
+            //CARS
+            var bike1 = new Bike() { LicensePlate = "1111AA-1", Brand = "Java", MaxSpeed = 120, MaxLoad = 150 };
+            var bike2 = new Bike() { LicensePlate = "2222AA-1", Brand = "Izh", MaxSpeed = 100, MaxLoad = 160 };
+            var bike3 = new Bike() { LicensePlate = "3333AA-1", Brand = "Minsk", MaxSpeed = 110, MaxLoad = 300, SidecarIsAble = true };
+            var passCar1 = new PassengerCar() { LicensePlate = "4444AA-1", Brand = "Geely", MaxSpeed = 160, MaxLoad = 500 };
+            var passCar2 = new PassengerCar() { LicensePlate = "5555AA-1", Brand = "Audi", MaxSpeed = 180, MaxLoad = 600 };
+            var passCar3 = new PassengerCar() { LicensePlate = "6666AA-1", Brand = "Mercedes", MaxSpeed = 210, MaxLoad = 700 };
+            var freight1 = new FreightCar() { LicensePlate = "7777AA-1", Brand = "Volvo", MaxSpeed = 120, MaxLoad = 10000 };
+            var freight2 = new FreightCar() { LicensePlate = "8888AA-1", Brand = "Scania", MaxSpeed = 140, MaxLoad = 12000 };
+            var freight3 = new FreightCar() { LicensePlate = "9999AA-1", Brand = "Renault", MaxSpeed = 160, MaxLoad = 13000, TrailerIsAble = true };
 
+            List<CarInfo> allVehicles = new List<CarInfo>()
+            {
+                bike1,
+                bike2,
+                bike3,
+                passCar1,
+                passCar2,
+                passCar3,
+                freight1,
+                freight2,
+                freight3
+            };
 
+            Console.WriteLine("All vehicles: ");
+            foreach (var vehicle in allVehicles)
+            {
+                vehicle.PrintVehicleInfo();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Vehicle with maximum load more than 10000 is: ");
+            var maxLoad = VehicleCapacity.VehicleFound(allVehicles, 10001);
+            foreach (var vehicleMax in maxLoad)
+            {
+                vehicleMax.PrintVehicleInfo();
+            }
         }
     }
 }
