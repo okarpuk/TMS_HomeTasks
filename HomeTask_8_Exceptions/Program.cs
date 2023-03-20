c676852c0af9809202bee54f1ec14267075871e8
okarpuk <oleg.karpuk.1982@gmail.com>
2023-03-20 10:01:22 +0300

    Program and Parameters added

--- HomeTask_8_Exceptions/Program.cs
+++ HomeTask_8_Exceptions/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Parameters.NewAutorization("Login", "Pas sw0rd", "Passw0rd");
         }
     }
 }
