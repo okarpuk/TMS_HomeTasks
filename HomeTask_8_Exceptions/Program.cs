167bea03809578fbc83130b60e8f809e9b7976fe
okarpuk <oleg.karpuk.1982@gmail.com>
2023-03-20 09:57:24 +0300

    HW 8 created

---
+++ HomeTask_8_Exceptions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeTask_8_Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ HomeTask_8_Exceptions/WrongPasswordException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_8_Exceptions
+{
+    internal class WrongPasswordException
+    {
+    }
+}
