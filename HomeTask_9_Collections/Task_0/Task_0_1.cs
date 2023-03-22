38a14e70051534777b0639bce9429777dec28ae8
okarpuk <oleg.karpuk.1982@gmail.com>
2023-03-22 19:44:33 +0300

    Task 0 added

---
+++ HomeTask_9_Collections/Task_0/Task_0_1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_9_Collections.Task_0
+{
+    internal class Task_0_1
+    {
+        public static void arrayException()
+        {
+            try
+            {
+                ArrayList list = new ArrayList();
+                object s = list[18];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error! Exception: {ex.Message}");
+            }
+        }
+    }
+}
