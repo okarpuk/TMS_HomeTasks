42f56cc8c5c42a8a19e25c73bb3a8fe094422c6f
okarpuk <oleg.karpuk.1982@gmail.com>
2023-03-29 19:52:02 +0300

    Task 1 Generic created

---
+++ HomeTask_10_Generic/Generic/GenericExecute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_10_Generic.Generic
+{
+    internal class GenericExecute
+    {
+        public static void MyGeneric()
+        {
+            Generic<string> myArray = new Generic<string>();
+            myArray.AddNewElement("1stElement");
+            myArray.AddNewElement("2ndElement");
+            myArray.AddNewElement("3rdElement");
+            myArray.AddNewElement("4thElement");
+            myArray.AddNewElement("5thElement");
+            Console.WriteLine(myArray.ArrayLength());
+            myArray.DeleteElement("3rdElement");
+            Console.WriteLine(myArray.ArrayLength());
+            myArray.FindElement(2);
+            myArray.FindElement(9);
+        }
+    }
+}
